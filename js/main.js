23fde05bc5e8106f3545509b6b39f81a740c5dae
Nikita Fedorov <nikfedoroff1997@yandex.ru>
2019-04-03 19:50:38 +0300

    Сверстана страница с адаптивом + JS +оптимизация карт

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ $(window).load(function () {
   });
 
   //Настройка Select
-  $('.select_checked').on('click', function(){
+  $('.select_checked').on('click', function () {
     $('.select__dropdown').toggleClass('select__dropdown_open');
   });
-  $('.select__option').on('click', function(){
+  $('.select__option').on('click', function () {
     var value = $(this).attr('data-value');
     $('#select-type').val(value);
     $('.select_checked').text(value);
@@ -26,8 +26,20 @@ $(window).load(function () {
   });
   $("a[href^='#']").click(function () {
     var _href = $(this).attr("href");
-    $('html, body').animate({ scrollTop: $(_href).offset().top -100 + "px"});
+    $('html, body').animate({
+      scrollTop: $(_href).offset().top - 100 + "px"
+    });
     retutn: false;
   });
   $('input[type="tel"]').mask("+7 (999) 999-99-99");
+  //Показывать карту только когда докрутили до неё
+  var reviews = $('.reviews');
+  var reviewsTop = reviews.offset().top;
+  $(window).bind('scroll', function () {
+    var windowTop = $(this).scrollTop();
+    if (windowTop > reviewsTop) {
+      $('#map').html('<script type = "text/javascript" charset = "utf-8" async src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A232d477d72a5de988300dae877713d01e4e3847c0bd54e6a920ce16cf33e7256&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>')
+      $(window).unbind('scroll')
+    }
+  });
 });
